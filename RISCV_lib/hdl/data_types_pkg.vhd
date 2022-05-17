76e32b4b0b0c2f4a2a076d8d4f3b1681052efb8e
Felix Brugger <FelixBrugger@tutanota.com>
2022-05-17 14:37:32 +0200

    bugfix

--- RISCV_lib/hdl/data_types_pkg.vhd
+++ RISCV_lib/hdl/data_types_pkg.vhd
@@ -7,27 +7,30 @@
 --
 -- using Mentor Graphics HDL Designer(TM) 2020.2 Built on 12 Apr 2020 at 11:28:22
 --
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-PACKAGE data_types IS
-  
+library ieee;
+use ieee.std_logic_1164.all;
+library RISCV_lib;
+use RISCV_lib.constants.all;
+
+package data_types is
+
   ------------------------------------------
   -- Types
   ------------------------------------------
   -- word in memory
-  subtype word is std_logic_vector(WORD_WIDTH-1 downto 0);
-  
+  subtype word is std_logic_vector(WORD_WIDTH - 1 downto 0);
+
   ------------------------------------------
   -- Signals
   ------------------------------------------
-  
+
   -- Determines the operation for alu
-  type alu_mode is (alu_add,alu_sub,
-                    alu_sll,alu_srl, alu_sra, alu_sll, alu_srl, 
-                    alu_and, alu_sub, alu_xor, 
-                    alu_slt, alu_sltu,
-                    alu_jal, alu_jalr, alu_bew, alu_bne, alu_blt, alu_bge, alu_bltu, alu_bgeu
-                  );
+  type alu_mode is (alu_add, alu_sub,
+    alu_sll, alu_srl, alu_sra, alu_sll, alu_srl,
+    alu_and, alu_sub, alu_xor,
+    alu_slt, alu_sltu,
+    alu_jal, alu_jalr, alu_bew, alu_bne, alu_blt, alu_bge, alu_bltu, alu_bgeu
+  );
   -- Dtermines memory operation mode
-  type mem_mode is (lb, lh, lw, lbu, lhu, sb, sh ,sw);
-END data_types;
+  type mem_mode is (lb, lh, lw, lbu, lhu, sb, sh, sw);
+end data_types;
