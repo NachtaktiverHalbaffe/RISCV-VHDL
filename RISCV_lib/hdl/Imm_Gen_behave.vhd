2e48abb62694279f9faf80467284248af8cf6569
Tizian Dirr <st149242@stud.uni-stuttgart.de>
2022-05-18 15:20:03 +0200

    Add Architectures for RF and Imm_Gen

--- RISCV_lib/hdl/Imm_Gen_behave.vhd
+++ RISCV_lib/hdl/Imm_Gen_behave.vhd
@@ -9,5 +9,20 @@
 --
 ARCHITECTURE behave OF Imm_Gen IS
 BEGIN
+    Imm_Gen: process(imm_type,op_code) is
+    begin
+        case imm_type is
+        when I_Type => 
+            dec_imm <= (others => opcode(31));
+            dec_imm <= op_code(31 downto 20);
+        when S_Type
+            dec_imm <= (others => opcode(31));
+            dec_imm <= op_code(31 downto 25) & op_code(11 downto 7);
+        when B_Type
+        when U_Type
+        when J_Type
+        when others => 
+            dec_imm <=(others => '0')
+
 END ARCHITECTURE behave;
 
--- RISCV_lib/hdl/RF_behave.vhd
+++ RISCV_lib/hdl/RF_behave.vhd
@@ -7,7 +7,21 @@
 --
 -- using Mentor Graphics HDL Designer(TM) 2020.2 Built on 12 Apr 2020 at 11:28:22
 --
+library ieee; use ieee.std_logic_1164.all;
+library RISCV_lib; use RISCV_lib.all;
+
 ARCHITECTURE behave OF RF IS
+    signal registers: rf_storage;
+
 BEGIN
+    dec_rs1 <= registers(to_integer(unsigned(sel_rs1)));
+    dec_rs2 <= registers(to_integer(unsigned(sel_rs2)));
+
+    wb: process(wb_data,wb_target_reg) is
+    begin
+        registers(to_integer(unsigned(wb_target_reg)))  <= wb_data;
+        registers(to_integer(unsigned(0))) <= (others => '0');
+    end process wb;
+
 END ARCHITECTURE behave;
 
