b8ed31c364a2b91ab320de79b94d77afa83e9aa2
Tizian Dirr <st149242@stud.uni-stuttgart.de>
2022-05-24 17:03:22 +0200

    Add mux_alu implementation

--- RISCV_lib/hdl/mux_alu_behave.vhd
+++ RISCV_lib/hdl/mux_alu_behave.vhd
@@ -9,5 +9,13 @@
 --
 ARCHITECTURE behave OF mux_alu IS
 BEGIN
+    MUX : process(ex_mux_alu_sel, ex_rs2, ex_imm) is
+    begin
+        if ex_mux_alu_sel = '0' then
+            alu_in_2 <= ex_rs2;
+        else
+            alu_in_2 <= ex_imm; 
+        end if;
+    end process;
 END ARCHITECTURE behave;
 
