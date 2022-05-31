bd2d57ee2ebccda37982453c2119b6d61bc37383
Tizian Dirr <st149242@stud.uni-stuttgart.de>
2022-05-31 16:41:41 +0200

    Add nicer way to add opcode

--- RISCV_lib/hdl/IM_behav.vhd
+++ RISCV_lib/hdl/IM_behav.vhd
@@ -12,31 +12,34 @@ use ieee.std_logic_1164.all;
 library RISCV_lib;
 use RISCV_lib.all;
 
-architecture behav of im is
+ARCHITECTURE behav OF im IS
 
-begin
-
-  rom : process (if_pc) is
+BEGIN
+ rom: process(if_pc) is
+    variable word_pc_int : integer := 0;
+    constant rom_content: im_rom_type := (
+      b"000000000001_00000_000_00101_00100_11",  --ADDI x5, x0, 1
+      b"000000000111_00000_000_00110_00100_11",  --ADDI x6, x0, 7
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00101_00110_000_00111_01100_11", --ADD x7, x5, x6
+      b"000000000010_00000_000_00101_00100_11",  --ADDI x5, x0, 2
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11", --NOP
+      b"0000000_00000_00000_000_00000_01100_11" --NOP
+    );
   begin
-    case to_integer(unsigned(if_pc)) is
-      when 0 =>
-        if_op_Code <= b"000000000001_00000_000_00101_00100_11"; --ADDI x5, x0, 1
-      when 4 =>
-        if_op_Code <= b"000000000011_00000_000_00110_00100_11"; --ADDI x6, x0, 3
-      when 8 =>
-        if_op_Code <= b"0000000_00000_00000_000_00000_01100_11"; --NOP
-      when 12 =>
-        if_op_Code <= b"0000000_00000_00000_000_00000_01100_11"; --NOP
-      when 16 =>
-        if_op_Code <= b"0000000_00000_00000_000_00000_01100_11"; --NOP
-      when 20 =>
-        if_op_Code <= b"0000000_00000_00000_000_00000_01100_11"; --NOP
-      when 24 =>
-        if_op_Code <= b"0000000_00101_00110_000_00111_01100_11"; --ADD x7, x5, x6
-      when 28 =>
-        if_op_Code <= b"000000000010_00000_000_00101_00100_11"; --ADDI x5, x0, 2
-      when others =>
-        if_op_Code <= b"0000000_00000_00000_000_00000_01100_11"; --NOP
-    end case;
+    word_pc_int := to_integer(unsigned(if_pc))/4;
+    if word_pc_int < rom_content'right then
+      if_op_code <= rom_content(  to_integer(unsigned(if_pc))/4 );
+    else
+      if_op_code <= b"0000000_00000_00000_000_00000_01100_11"; --NOP
+    end if;
   end process rom;
-end architecture behav;
+
+END ARCHITECTURE behav;
